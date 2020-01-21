32f835d0b8a7e7f20cdb279b20cfe264baaa28a0
Kartik <59439483+Kartik-DEV31@users.noreply.github.com>
2020-01-21 08:22:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+		
+
 
 
 
@@ -10,29 +12,62 @@ function validation(){
 
 	var user = document.form.username.value
 	var pass = document.form.passwd.value
-
+	
 	var passwd= pass.length
 
-	var country =document.form.country.value
-	var gender=document.form.gender.value
-	var language=document.form.language.value
+	var address=document.form.address.value
+
+	
 
 	if (user==null || user=="") 
 	{
-		alert ("please Enter a User Name");
+					alert ("please Enter a User Name");
+				
 
 		return false;
 	}
 
 
-	/*if (passwd!=null || passwd!="") {
+	if (passwd!=null || passwd!="") {
 
 		 if (passwd<7 || passwd>8){
-		 	alert("please Enter a password with no. of characters in range 7<=c<=8");
+		 	alert("please Enter a password with no. of characters in range 7<=c<=8 ");
 		 	return false;
 		 }
 
-	}*/
 	}
 
 
+
+	if (address==null || address=="") 
+	{
+		alert ("please Enter your address");
+
+		return false;
+	}
+
+
+	}
+
+
+
+	function hover(){
+		
+		
+
+					document.getElementById('reg').style.color="green";
+					return false;
+		
+
+
+	}
+
+
+function nothover(){
+		
+
+					document.getElementById('reg').style.color="black";
+					return false;
+		
+
+	}
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Problem-Statement-2
+
+	Registration Form
+
+	Tecnology(ies) used: HTML/CSS and JavaScript.
+
+
+important note:	The website works best with resolution = 1366 X 768.
